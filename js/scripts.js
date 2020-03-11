4e57cc35e70c94a1898b9c93bf39d91a2de4e414
lft <lucasftrimble@gmail.com>
2020-03-10 18:58:54 -0700

    adds first choices

---
+++ js/scripts.js
@@ -0,0 +1,9 @@
+var enterAlley = confirm("Say, son what'cha got over there? How about takin' Ladybird out for a walk?")
+
+if(enterAlley == true)  {
+  var walkDog = prompt("Bobby your mother raised you right, boy I tell you w'hat!")
+}
+
+else(seeConnie == true)  {
+
+}
